1fc2a64b4c03c3849ec199f54993eab5c12bc935
Jacob  Bauer <jacobbauer@Jacobs-MacBook-Air.local>
2022-05-13 15:52:53 -0500

    added dynamic HTML

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
 // const pageHTML = generatePage(name, github);
 
 
-// fs.writeFile('./index.html', pageHTML, err => {
-//     if (err) throw new Error(err);
+fs.writeFile('./index.html', pageHTML, err => {
+    if (err) throw new Error(err);
 
 //     console.log('Portfolio complete! Check out index.html to see the output!');
-// });
+});
 
-// printProfileData(profileDataArgs);
+printProfileData(profileDataArgs);
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -146,5 +146,11 @@ if (!portfolioData.projects) {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+         const pageHTML = generatePage(mockData);
+
+        // fs.writeFile('./index.html', pageHTML, err => {
+        //   if (err) throw new Error(err);
+
+        //   console.log('Page created! Check out index.html in this directory to see it!');
+        // });
     });
